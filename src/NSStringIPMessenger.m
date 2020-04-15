3513e5f0b5a66313e0a00a884618e072b50bd2f9
plmusan <w1989123m@126.com>
2020-04-15 10:53:58 +0800

    把ShiftJIS编码改为gbk编码

--- src/NSStringIPMessenger.m
+++ src/NSStringIPMessenger.m
@@ -20,7 +20,7 @@
 // IPMessenger用送受信文字列変換（C文字列→NSString)
 - (id)initWithSJISString:(const char*)cString
 {
-	return [self initWithCString:cString encoding:NSShiftJISStringEncoding];
+	return [self initWithCString:cString encoding:-2147482063];
 }
 
 // IPMessenger用送受信文字列変換（NSString→C文字列)
@@ -29,16 +29,16 @@
 	// SJISの場合、'¥'は'\'に変換しておかないと文字化けする
 	NSString* str = [self stringByReplacingOccurrencesOfString:@"¥"
 													withString:@"\\"];
-	if (![str canBeConvertedToEncoding:NSShiftJISStringEncoding]) {
+	if (![str canBeConvertedToEncoding:-2147482063]) {
 		// 変換できない文字がある場合は安全な方法で変換する
-		NSData*			data1 = [str dataUsingEncoding:NSShiftJISStringEncoding
+		NSData*			data1 = [str dataUsingEncoding:-2147482063
 								  allowLossyConversion:YES];
 		NSMutableData*	data2 = [[data1 mutableCopy] autorelease];
 		[data2 appendBytes:"\0" length:1];
 		return (const char*)[data2 bytes];
 	}
 	// 変換できるならそのまま
-	return [str cStringUsingEncoding:NSShiftJISStringEncoding];
+	return [str cStringUsingEncoding:-2147482063];
 }
 
 @end
